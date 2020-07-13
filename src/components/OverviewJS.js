799d4e08112c73d337b74601b9032289f5eba751
Filip Andjelic <filip.andjelic.private@gmail.com>
2020-07-13 18:30:30 +0200

    Preparing for new tasks...

---
+++ src/components/OverviewJS.js
@@ -0,0 +1,49 @@
+// External dependencies
+import React from "react";
+
+
+export default class OverviewJS extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div id="overview-js" className="flex-column">
+            <div id="header" className="row center border-b-1 padding-v-40 margin-h-30 large-text bold">
+                Oykos Development Guidelines
+            </div>
+
+            <div id="mainPage" className="column align-stretch margin-v-40 margin-h-30">
+                <ul className="large-text">
+                    <li>Chapter #2 - Way of JS</li>
+                </ul>
+
+                <div id="margin-padding" className="row justify-start align-stretch padding-h-30 margin-t-30">
+                    <div id="left" className="border-r-1 min-max-w-50perc padding-20">
+                        <ul className="big-text">
+                            <li>Selecting DOM elements</li>
+                        </ul>
+
+                        <div className="column align-stretch">
+                            <p>
+                                For styling margins and padding we use different combination of classes. See example below.
+                            </p>
+                            <code className="margin-10">.margin-[value]</code>
+                            <p>
+                                This sets all four margins to one of the predefined values.
+                                We can also set margins individually (top, right, bottom, left) by using class
+                                names...
+                            </p>
+                        </div>
+                    </div>
+
+                    <div id="right" className="column align-stretch justify-start padding-20 grow-1">
+                        <div className="flex center margin-20"
+                             style={{ backgroundColor: "#54ca54" }}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
+    }
+}
---
+++ src/components/Practise.js
@@ -0,0 +1,20 @@
+// External dependencies
+import React from "react";
+
+export default class Practise extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div id="practise" className="flex-column">
+            <div id="header" className="row center border-b-1 padding-v-40 margin-h-30 large-text bold">
+                Practise Page
+            </div>
+
+            <div id="mainPage" className="column align-stretch margin-v-40 margin-h-30">
+
+            </div>
+        </div>);
+    }
+}
