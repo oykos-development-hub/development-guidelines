987ff9ca2b52fc413b04ec99204fa71986c0bb1d
Djordje <djordje-globarevic@oykos-hub.com>
2020-07-27 15:57:13 +0200

    Router

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,15 @@ export default class App extends React.Component {
 
     render() {
         return (<Router>
+                <Route>
+                    <About />
+                </Route>
+                <Route>
+                    <Stats />
+                </Route>
+                <Route>
+                    <Dashboard />
+                </Route>
             <Switch>
                 <Route exact path="/practise">
                     <Practise />
@@ -37,4 +46,15 @@ export default class App extends React.Component {
             </Switch>
         </Router>);
     }
+}
+function About(){
+    return <h1>About</h1>
+}
+
+function Stats(){
+    return <h1>Stats</h1>
+}
+
+function Dashboard(){
+    return <h1>Dashboard</h1>
 }
