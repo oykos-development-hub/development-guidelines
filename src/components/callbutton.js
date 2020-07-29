c85d1d0f164f19eb6172d0ad494f22b78cfc4490
teodora01 <teodora-popovic@oykos-hub.com>
2020-07-29 20:00:06 +0200

    ReactJS

---
+++ src/components/callbutton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class CallButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+                  <button  className="border-radius-top-right-6 bg-info-3 w-100perc h-100perc column center light-5">{this.props.text}</button>
+        );
+    }
+}
---
+++ src/components/image.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Image extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <div id="Image" className="border-radius-top-left-6 h-100perc w-25perc bg-size-cover bg-no-repeat bg-position-50-perc">
+            
+          <img src={require('../img/main-bg.jpg')} className="h-100perc w-400"></img>
+          
+            
+        </div>);
+    }
+}
