25f9737db4d576b85ffca34ad3f26ae54d859942
Djordje <djordje-globarevic@oykos-hub.com>
2020-07-31 11:09:13 +0200

    App

--- src/components/App.js
+++ src/components/App.js
@@ -10,27 +10,25 @@ import "../style/main.scss";
 import Overview from './Overview';
 import OverviewJS from "./OverviewJS";
 import Practise from "./Practise";
-
+import {StoreService} from '../services/store.service';
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        StoreService.initialize();
     }
-
+    
     render() {
         return (<Router>
             <Switch>
                 <Route exact path="/practise">
                     <Practise />
                 </Route>
-
                 <Route exact path="/js">
                     <OverviewJS/>
                 </Route>
-
                 <Route exact path="/css">
                     <Overview/>
                 </Route>
-
                 <Route exact path="/">
                     <Overview/>
                 </Route>
